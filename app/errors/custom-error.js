0e23cc5ee883becd1ba174f3890f9ce84f5dd01b
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-05-17 15:32:47 +0430

    Implement structure

---
+++ app/errors/custom-error.js
@@ -0,0 +1,10 @@
+'use strict'
+
+class CustomAPIError extends Error {
+    constructor(message, statusCode) {
+        super(message)
+        this.statusCode = statusCode
+    }
+}
+
+module.exports = CustomAPIError
---
+++ app/middlewares/not-found.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const notFound = (req, res) => {
+    res.status(404).send('Route does not exist')
+}
+
+module.exports = notFound
