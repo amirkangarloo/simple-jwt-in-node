0611800de8050363ffd1952a89c5137e1a7a0530
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-05-19 11:39:19 +0430

    Implement connect DB

--- app/db/connect.js
+++ app/db/connect.js
@@ -1,1 +1,9 @@
-'use strict'    
+'use strict'
+
+const mongoose = require('mongoose')
+
+const connectDB = (url, dbName) => {
+    return mongoose.connect(`${url}/${dbName}`)
+}
+
+module.exports = connectDB
