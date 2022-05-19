83a075e236b813626e2bafd5ac7745389d99a3f4
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-05-19 11:22:37 +0430

    Implement server.js and start app

--- app/index.js
+++ app/index.js
@@ -1,1 +1,26 @@
 'use strict'
+
+const express = require('express')
+const app = express()
+
+const notFoundMiddleware = require('./middlewares/not-found')
+const errorHandlerMiddleware = require('./middlewares/error-handler')
+const { model } = require('mongoose')
+
+// middleware
+app.use(express.static('public'))
+app.use(express.json())
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
+module.exports = async (port) => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server is listennong on port ${port}...`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 'use strict'
+
+require('dotenv').config()
+require('express-async-errors')
+
+const startApp = require('./app')
+
+startApp(process.env.APP_PORT)
