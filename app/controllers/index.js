51e84a024adf6a450615e9c4bcd3fd56528940e8
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-05-21 11:54:36 +0430

    Implement create a token by JWT on login route

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,37 @@
 'use strict'
 
+const CustomeAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
+
 exports.login = async (req, res, next) => {
-    res.send('Fack login/Register/Signup Route')
+    const {
+        username,
+        password
+    } = req.body
+
+    if (!username || !password) {
+        throw new CustomeAPIError('username or password is incorrect', 403)
+    }
+
+    const id = new Date().getDate()
+    const token = jwt.sign({
+        id,
+        username
+    },
+    process.env.APP_SECRET,
+    {
+        expiresIn: '10d'
+    })
+
+    res.status(200).send({
+        msg: 'user create',
+        token
+    })
 }
 
 exports.dashboard = async (req, res, next) => {
-    const luckyNumber = Math.floor(Math.random()*100)
-    res. status(200).send({
+    const luckyNumber = Math.floor(Math.random() * 100)
+    res.status(200).send({
         msg: "Hello Amir",
         secret: `Here is your authorized data, you lucky number is ${luckyNumber}`
     })
